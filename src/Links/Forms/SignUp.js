61b5f73004bb94fd63de54a9888c4326075bb8e8
Saswati Priyadarshini <spriyadarshini@crosslight.io>
2018-07-24 00:01:42 +0530

    state practice

--- src/Links/Forms/SignUp.js
+++ src/Links/Forms/SignUp.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react';
 import FormElement from './FormElement';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import './SignUp.css';
 
 class Forms extends Component{
   render(){
     return(
-      <div>
-        <FormElement />
+      <div className='form-container'>
+        <Form>
+          <FormElement
+            inputname='full-name'
+            type='text'
+            placeholder="Enter Your Full Name"
+            name='Full Name'
+            className='form-control'
+            onChange=''
+          />
+
+        <FormElement
+          inputname='email'
+          type='text'
+          placeholder="Type your Email"
+          name='Email'
+          className='form-control'
+          onChange=''
+        />
+
+        <FormElement
+          inputname='password'
+          type='password'
+          placeholder="Type Your Password"
+          name='password'
+          className='form-control'
+          onChange=''
+        />
+          <button className='btn-signUp'>Sign Up</button>
+        </Form>
       </div>
     );
   }
